ee1356ca51595fb7c89950a647c6b7209eda47b7
Makankosappo-learnIT <84960911+Makankosappo-learnIT@users.noreply.github.com>
2021-05-31 22:40:53 +0800

    Add files via upload

--- routes/form.js
+++ routes/form.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const tools = require('../tools');
 
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../public', 'form.html'));
@@ -8,6 +9,9 @@ router.get('/', function(req, res){
 
 router.post('/', function(req, res){
     console.log(req.body);
+    var name = req.body.First_Name + " " + req.body.Last_Name; 
+    console.log(name);
+    tools.input_data(name, req.body.Email);
     res.redirect('./form');
 });
 
